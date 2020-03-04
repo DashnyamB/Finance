9410e395a5f66d258b5fdc1eba7af18c1210ca06
Dashnyam <lord_dashka277@yahoo.com>
2020-03-04 18:24:34 +0800

    finance moduli hiij ehelesen

--- app.js
+++ app.js
@@ -20,10 +20,30 @@ var uiController = (function() {
   };
 })();
 //Controller which works with finance
-var financeController = (function() {})();
+var financeController = (function() {
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Expence = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var data = {
+    allItems: {
+      inc: [],
+      exp: []
+    },
+    totals: {
+      inc: 0,
+      exp: 0
+    }
+  };
+})();
 //Controller that connects app
 var appController = (function(uiController, financeController) {
-  var DOM = uiController.getDOMstrings();
   var ctrlAddItem = function() {
     //1. get datas from desktop
     console.log(uiController.getInput());
@@ -32,11 +52,22 @@ var appController = (function(uiController, financeController) {
     //4. Calculate finance
     //5. Display a calculated data in web.
   };
-  document.querySelector(DOM.addBtn).addEventListener("click", function() {
-    ctrlAddItem();
-  });
+  var setUpEventListener = function() {
+    var DOM = uiController.getDOMstrings();
+    document.querySelector(DOM.addBtn).addEventListener("click", function() {
+      ctrlAddItem();
+    });
 
-  document.addEventListener("keypress", function(event) {
-    if (event.keyCode === 13 || event.which === 13) ctrlAddItem();
-  });
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13 || event.which === 13) ctrlAddItem();
+    });
+  };
+
+  return {
+    init: function() {
+      setUpEventListener();
+    }
+  };
 })(uiController, financeController);
+
+appController.init();
